b2e17ee265fa4c7d8619a6a56b3449eb57ce09fa
suraj <suraj@example.com>
2025-02-10 14:55:19 +0100

    removed Modules/EC2_Instance file

--- Modules/EC2_Instance/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-    description = "Display the Public IP of EC2 instance"
-    value = aws_instance.mywebserver.public_ip  
-}
-output "account_username" {
-    description = "Display the account username of the EC2 instance"
-    value       = var.ami_id == "ami-12345678" ? "ec2-user" : "ubuntu"
-  
-}
